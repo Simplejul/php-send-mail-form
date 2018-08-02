d734871fcda2f27d7b4069b220e873e7cc708edb
Simplejul <simplejul@protonmail.com>
2018-08-02 14:16:13 +0200

    end

---
+++ index.html.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Send mail</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+</head>
+<body>
+    <form action="/index.html.php" method="post" class="form">
+        
+        <div class="form">
+            <label for="email">
+                <h3>Enter your email: </h3>
+                <input type="email" name="email" id="email" required>
+            </label>
+        </div>
+        <div class="form">
+            <label for="msg">
+                <h3>Enter your message:</h3>
+                <textarea id="advanced" name="msg" rows="3" cols="33" maxlength="200" wrap="hard"></textarea>
+            </label>
+        </div>
+        <div class="form">
+            <input type="submit" value="Send">
+        </div>
+    </form>
+    <?php
+        if ($_POST['msg'] && $_POST["email"]){
+            $subject = 'no subject';
+            $headers = 'From: webmaster@example.com' . "\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+
+            mail($_POST["email"], $subject, $_POST['msg'], $headers);
+        }
+    ?>
+</body>
+</html>
